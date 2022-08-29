ca7b138014954b0381df867137e5c6f20d199168
RitikSinha <ritikk910@gmail.com>
2022-08-29 21:16:08 +0530

    homework 5 done

---
+++ 05split.js
@@ -0,0 +1,7 @@
+/**
+5. Split the string into an array using split() method
+ * 
+ */
+var myString = "hello javascript words in array";
+var myStringArr = myString.split(" ");
+console.log(myStringArr);
