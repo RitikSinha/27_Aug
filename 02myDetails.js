f0a051cf8460810a1f3a41284bd13c9cd4a121a7
RitikSinha <ritikk910@gmail.com>
2022-08-29 20:37:04 +0530

    2nd homework done

---
+++ 02myDetails.js
@@ -0,0 +1,11 @@
+/**
+ * 2. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+ */
+var firstName = "Ritik";
+var lastName = "Kumar";
+var isMarried = false;
+var country = "India";
+var age = 22;
+console.log(
+  `Hello I'm ${firstName} ${lastName} a ${age} years old programmer. My marital status is ${isMarried}. I live in ${country}.`
+);
