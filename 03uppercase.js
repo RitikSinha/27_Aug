77bec2d19ad0cff60a1129ef72c5b2eb7ec6b8ef
RitikSinha <ritikk910@gmail.com>
2022-08-29 20:42:31 +0530

    homework 3 done

---
+++ 03uppercase.js
@@ -0,0 +1,8 @@
+/*
+3. Decalre a varibale and assign value to it and change all the string characters to capital letters using toUpperCase() method.
+*/
+var sentence = "hello world";
+var uppercaseSentence = sentence.toUpperCase();
+console.log(
+  `I changed ${sentence} (lower case) to ${uppercaseSentence} (upper case)`
+);
