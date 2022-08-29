243dce3d608c2fab2104c3de669e2b110078942a
RitikSinha <ritikk910@gmail.com>
2022-08-29 21:38:22 +0530

    homework 7 done

---
+++ 07LIO.js
@@ -0,0 +1,8 @@
+/**
+7. Use lastIndexOf to determine the position of the last occurrence of a script.
+ * 
+ */
+
+var sentence = "I wrote a script which is a simple script to scrap web";
+var index = sentence.lastIndexOf("script");
+console.log(index);
