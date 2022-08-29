f25b5ba36ad778e45b1a28e5ba56db9196b17365
RitikSinha <ritikk910@gmail.com>
2022-08-29 21:22:38 +0530

    homework6

---
+++ 06aplit.js
@@ -0,0 +1,7 @@
+/**
+6. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+ * 
+ */
+var maang = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+var maangArr = maang.split(",");
+console.log(maangArr);
