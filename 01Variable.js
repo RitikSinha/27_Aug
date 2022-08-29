445556910a770e96d91d7ed3d00eca24e5c7d3e7
RitikSinha <ritikk910@gmail.com>
2022-08-29 20:28:07 +0530

    homework 1

---
+++ 01Variable.js
@@ -0,0 +1,11 @@
+/*Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.*/
+var myName = "Ritik Kumar";
+var isStudent = true;
+var college = undefined;
+var hate = null;
+console.log(`
+${myName} is ${typeof myName}
+${isStudent} is ${typeof isStudent}
+${college} is ${typeof college}
+${hate} is ${typeof hate}
+`);
