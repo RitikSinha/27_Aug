7a9ef5d84a90df4548c1c4398c73d29c25838ccf
RitikSinha <ritikk910@gmail.com>
2022-09-01 23:16:55 +0530

    31 done

--- 31.js
+++ 31.js
@@ -1,3 +1,23 @@
 /**
  * 31. Sort the webTechs array and mernStack array
  */
+let webTechs = [
+  "Html",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Vue",
+  "Typescript",
+  "Angular",
+  "Deno",
+  "Node js",
+  "Gatsby js",
+  "Next js",
+  "Nuxt js",
+  "express",
+  "Postgress",
+  "mongo db",
+  "Nginx",
+];
+let mernStack = ["React", "Mongo DB", "Node js", "Express"];
+console.log("web Tech Sorted", webTechs.sort(), "Mern Stack", mernStack.sort());
